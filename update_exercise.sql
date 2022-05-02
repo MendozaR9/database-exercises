3961b2197507f1725d9d8f2535861bf96dbf30d2
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-05-02 15:25:50 -0500

    update albums table

---
+++ update_exercise.sql
@@ -0,0 +1,19 @@
+use codeup_test_db;
+select name
+from albums;
+update albums
+set sales = sales*10;
+select * from albums;
+
+select name from albums where release_date <1980;
+update albums
+set release_date = release_date-100
+where release_date<1980;
+select name, release_date from albums where release_date <1980;
+
+
+select name, artist from albums where  artist = 'Michael Jackson ';
+update albums
+set artist = 'Peter Jackson'
+where artist = 'Michael Jackson ';
+select name, artist from albums where  artist = 'Peter Jackson';
