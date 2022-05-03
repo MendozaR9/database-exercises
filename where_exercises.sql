3ad9e6b9dc0a5bb184a20b6ded2da2d4f06d68e2
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-05-03 10:33:14 -0500

    finish part 2 clauses exercies

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,9 @@
 use employees;
 select *
 from employees
-where first_name in ('Irena', 'Vidya', 'Maya');
+where (first_name = 'Irena' or first_name = 'Vidya'
+or first_name = 'Maya')
+and gender = 'M';
 
 select *
 from employees
@@ -10,3 +12,15 @@ where last_name like 'k%';
 select *
 from  employees
 where last_name like '%q%';
+
+select *
+from employees
+where last_name like 'E%' or last_name like '%E';
+
+select *
+from employees
+where last_name like 'E%' and last_name like '%E';
+
+select *
+from employees
+where last_name like '%q%' and last_name not like '%qu%';
